25ae31f8621ece69cd658031ba85274c93da386a
Chayathon <gusgus157903@gmail.com>
2024-10-08 22:12:21 +0700

    update navbar

--- frontend/src/app/admin/layout.js
+++ frontend/src/app/admin/layout.js
@@ -2,13 +2,13 @@ import AdminNavbar from "./components/Navbar"
 
 export default function AdminLayout({
     children, // will be a page or nested layout
-  }) {
-    return (
-        <section>
-            {/* Include shared UI here e.g. a header or sidebar */}
-            
-            <AdminNavbar />
-            {children}
-        </section>
-    )
-  }
+})  {
+        return (
+            <section>
+                {/* Include shared UI here e.g. a header or sidebar */}
+                
+                <AdminNavbar />
+                {children}
+            </section>
+        )
+    }
