b9c3c90a471a974eb738a9f7c438e44519d9eec3
Chayathon <gusgus157903@gmail.com>
2024-10-08 22:32:12 +0700

    add Navbar.js

---
+++ frontend/src/app/components/Navbar.js
@@ -0,0 +1,37 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation'
+import { Text, Button } from '@chakra-ui/react'
+import { toast } from 'react-toastify';
+
+function UserNavbar() {
+    const router = useRouter()
+
+    const logout = () => {
+        Cookies.remove('UserData');
+        Cookies.remove('Token');
+
+        toast.success("ออกจากระบบแล้ว")
+        router.push('/')
+    }
+
+    return (
+        <nav className='bg-[#333] text-white p-5'>
+            <div className='container mx-auto'>
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <Text>Precision Agriculture</Text>
+                    </div>
+                    <ul className='flex'>
+                        <li className='mx-3'><Button onClick={logout}>Log out</Button></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default UserNavbar
