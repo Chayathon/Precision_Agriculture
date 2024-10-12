0f2ed83dbd3dcea8eaf904987e1a5b1bd422cd59
Chayathon <gusgus157903@gmail.com>
2024-10-13 00:03:53 +0700

    update chakra to nextui

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,13 +4,13 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Cookies from 'js-cookie';
-import { Flex, Card, CardHeader, CardBody, Heading, FormLabel, Input, InputGroup, InputRightElement, Button, Stack, Text } from '@chakra-ui/react'
+import {Card, CardHeader, CardBody, CardFooter, Input, Button} from "@nextui-org/react";
 import { toast } from 'react-toastify';
 
 function Page() {
     const router = useRouter()
-    const [show, setShow] = useState(false)
-    const handleClick = () => setShow(!show)
+    const [isVisible, setIsVisible] = React.useState(false);
+    const toggleVisibility = () => setIsVisible(!isVisible);
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
@@ -55,41 +55,39 @@ function Page() {
         }
     }
 
-    return(
-        <Stack className="w-[100vw] h-[100vh]">
+    return (
+        <div className='grid w-[100vw] h-[100vh]'>
             <Card className="m-auto w-1/3 drop-shadow-2xl bg-blend-darken">
-                <CardHeader>
-                    <Heading size='lg'>เข้าสู่ระบบ</Heading>
+                <CardHeader className='text-2xl font-bold justify-center'>
+                    เข้าสู่ระบบ
                 </CardHeader>
                 <CardBody>
                     <form onSubmit={handleSubmit}>
-                        <FormLabel>ชื่อผู้ใช้</FormLabel>
-                        <Input onChange={(e) => setUsername(e.target.value)} placeholder='ชื่อผู้ใช้' size='md' />
-                        <br /><br />
-                        <FormLabel>รหัสผ่าน</FormLabel>
-                        <InputGroup size='md'>
-                            <Input
-                                onChange={(e) => setPassword(e.target.value)}
-                                pr='4.5rem'
-                                type={show ? 'text' : 'password'}
-                                placeholder='รหัสผ่าน'
-                            />
-                            <InputRightElement width='4.5rem'>
-                                <Button h='1.75rem' size='sm' onClick={handleClick}>
-                                    {show ? 'ซ่อน' : 'แสดง'}
+                        <Input onChange={(e) => setUsername(e.target.value)} type='text' label='ชื่อผู้ใช้' />
+                        <Input
+                            onChange={(e) => setPassword(e.target.value)}
+                            label="รหัสผ่าน"
+                            endContent={
+                                <Button type="button" size="sm" className='bg-gray-300' onClick={toggleVisibility} aria-label="toggle password visibility">
+                                {isVisible ? (
+                                    'ซ่อน'
+                                ) : (
+                                    'แสดง'
+                                )}
                                 </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                        <br />
-                        <Button colorScheme='green' type='submit' className='w-full'>เข้าสู่ระบบ</Button><br />
-                        <Flex className='justify-between'>
-                            <Text>ยังไม่มีบัญชี? <Link href='/register' className='text-blue-500 hover:underline'>สมัครสมาชิก</Link></Text>
-                            <Link href='/forgotPassword' className='text-blue-600 hover:underline'>ลืมรหัสผ่าน</Link>
-                        </Flex>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            className='my-4'
+                        />
+                        <Button type='submit' color='success' className='w-full'>เข้าสู่ระบบ</Button>
                     </form>
                 </CardBody>
+                <CardFooter className='flex justify-between'>
+                    <p>ยังไม่มีบัญชี? <Link href='/register' className='text-blue-500 hover:underline'>สมัครสมาชิก</Link></p>
+                    <Link href='/forgotPassword' className='text-blue-600 hover:underline'>ลืมรหัสผ่าน</Link>
+                </CardFooter>
             </Card>
-        </Stack>
+        </div>
     )
 }
 
