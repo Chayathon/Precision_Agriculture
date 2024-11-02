df592d11ee368645768384340942d938ffa54d5f
Chayathon <gusgus157903@gmail.com>
2024-11-02 17:40:04 +0700

    update dashboard

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,11 +1,58 @@
 import React from 'react'
-import { FaHouse } from "react-icons/fa6";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 function Page() {
     return (
-        <div>
-            <div className='flex justify-center py-6'>
-                <FaHouse className='text-4xl' /><p className='text-4xl font-bold'> Dashboard Page</p>
+        <div className='m-4'>
+            <div className='grid grid-cols-6 gap-4'>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>อายุ (วัน)</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>30</p>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>อุณหภูมิ (°C)</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>25</p>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>ความชื้น (%)</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>90</p>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>ไนโตรเจน ()</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>70</p>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>ฟอสฟอรัส ()</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>70</p>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardHeader className='flex justify-center'>
+                        <p className='text-gray-500'>โพแทสเซียม ()</p>
+                    </CardHeader>
+                    <CardBody>
+                        <p className='text-center text-6xl font-bold'>70</p>
+                    </CardBody>
+                </Card>
             </div>
         </div>
     )
