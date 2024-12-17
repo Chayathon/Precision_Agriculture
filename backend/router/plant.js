c572506fc3f2e76bb5bde3527e75627ce0db8a1a
Adithep.T <jameza_love02@hotmail.com>
2024-12-17 22:38:13 +0700

    addFactor_Nutrient

--- backend/router/plant.js
+++ backend/router/plant.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const { authIsCheck, isAdmin } = require("../middleware/auth");
 
 router.get('/listPlant', async (req, res) => {
     try {
@@ -12,30 +14,6 @@ router.get('/listPlant', async (req, res) => {
             res.status(200).json({
                 message: "Get Plant",
                 resultData: listPlant,
-            });
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Server Error');
-    }
-});
-
-router.get("/getPlant/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const getPlant = await prisma.plant.findMany({
-            where: {
-                user_id: Number(id),
-            },
-            select: {
-                plantname: true,
-            },
-        });
-
-        if (getPlant) {
-            res.status(200).json({
-                message: "Get Plant by ID",
-                resultData: getPlant,
             });
         }
     } catch (err) {
@@ -79,6 +57,51 @@ router.post('/createPlant', async (req, res) => {
     }
 });
 
+router.get("/getPlant/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const getPlant = await prisma.plant.findMany({
+            where: {
+                user_id: Number(id),
+            },
+            select: {
+                plantname: true,
+            },
+        });
+
+        if (getPlant) {
+            res.status(200).json({
+                message: "Get Plant by ID",
+                resultData: getPlant,
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+// router.get("/getPlant/:id", async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const getPlant = await prisma.plant.findFirst({
+//             where: {
+//                 id: Number(id),
+//             },
+//         });
+
+//         if (getPlant) {
+//             res.status(200).json({
+//                 message: "Plant Plant by ID",
+//                 resultData: getPlant,
+//             });
+//         }
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Server Error');
+//     }
+// });
+
 router.put('/updatePlant/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -114,6 +137,70 @@ router.delete('/deletePlant/:id', async (req, res) => {
         if(delPlant) {
             res.status(200).json({
                 message: 'Plant deleted successfully',
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/createFactor', async (req, res) => {
+
+    const { ph, temperature, humidity, lightIntensity, salinity, plantId } = req.body;
+
+    console.log(req.body);
+
+    if (!plantId) {
+        return res.status(400).json({ message: "Missing plantId" });
+    }
+
+    try {
+        const createFactor = await prisma.p_factor.create({
+            data: {
+                pH: parseFloat(ph), // แปลงเป็นตัวเลข
+                temperature: parseFloat(temperature), // แปลงเป็นตัวเลข
+                humidity: parseFloat(humidity), // แปลงเป็นตัวเลข
+                lightIntensity: parseFloat(lightIntensity), // แปลงเป็นตัวเลข
+                salinity: parseFloat(salinity), // แปลงเป็นตัวเลข
+                plant_id: plantId,  // ใช้ plantId ที่รับมา
+            }
+        });
+
+        if(createFactor) {
+            res.status(200).json({
+                message: 'Factor created successfully',
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/createNutrient', async (req, res) => {
+
+    const { nitrogen, phosphorus, potassium, plantId } = req.body;
+
+    console.log(req.body);
+
+    if (!plantId) {
+        return res.status(400).json({ message: "Missing plantId" });
+    }
+
+    try {
+        const createNutrient = await prisma.p_nutrient.create({
+            data: {
+                nitrogen: parseFloat(nitrogen), // แปลงเป็นตัวเลข
+                phosphorus: parseFloat(phosphorus), // แปลงเป็นตัวเลข
+                potassium: parseFloat(potassium), // แปลงเป็นตัวเลข
+                plant_id: plantId,  // ใช้ plantId ที่รับมา
+            }
+        });
+
+        if(createNutrient) {
+            res.status(200).json({
+                message: 'Nutrient created successfully',
             });
         }
     } catch (err) {
--- frontend/src/app/components/ModalFactor_Nutrient.js
+++ frontend/src/app/components/ModalFactor_Nutrient.js
@@ -1,189 +1,231 @@
-import { useState } from 'react';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input, DateInput, useDisclosure, Tabs, Tab, Card, CardBody } from '@nextui-org/react';
-import { CalendarDate } from "@internationalized/date";
+import { useState, useEffect } from 'react'
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input, useDisclosure, Tabs, Tab } from '@nextui-org/react';
 import { toast } from 'react-toastify';
-import Cookies from 'js-cookie';
 
-function ModalFactor_Nutrient({ isOpen, onOpenChange, setRefresh }) {
+function ModalFactor_Nutrient({ isOpen, onOpenChange, setRefresh, id }) {
 
-  const [nitrogen, setNitrogen] = useState('')
-  const [phosphorus, setPhosphorus] = useState('')
-  const [potassium, setPotassium] = useState('')
+  //p_factor
+  const [ph, setPh] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [humidity, setHumidity] = useState('');
+  const [lightIntensity, setLightIntensity] = useState('');
+  const [salinity, setSalinity] = useState('');
 
-  const [ph, setPh] = useState('')
-  const [temperature, setTemperature] = useState('')
-  const [humidity, setHumidity] = useState('')
-  const [lightIntensity, setLightIntensity] = useState('')
-  const [salinity, setSalinity] = useState('')
+  //p_nutrient
+  const [nitrogen, setNitrogen] = useState('');
+  const [phosphorus, setPhosphorus] = useState('');
+  const [potassium, setPotassium] = useState('');
 
-   // Handle Submit สำหรับ Form 1
-  const handleSubmitFactor = async (e) => {
-    e.preventDefault();
+  const [plantId, setPlantId] = useState('');  // เก็บ id ของพืช
+  const [plantName, setPlantName] = useState('');
 
-    if (!nitrogen || !phosphorus || !potassium) {
-      toast.error("กรุณากรอกข้อมูลให้ครบทุกช่อง!");
-      return;
+  useEffect(() => {
+    if(isOpen) {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`http://localhost:4000/api/getPlant/${id}`);
+                    
+                if (!res.ok) {
+                    throw new Error("Failed to fetch");
+                }
+
+                const data = await res.json();
+                setPlantName(data.resultData.plantname);
+                setPlantId(data.resultData.id);     // เก็บ id ของพืช
+            } catch (err) {
+                console.error("Error fetching data: ", err);
+            }
+        }
+
+        fetchData()
     }
+}, [isOpen])
 
-    try {
-      const res = await fetch("http://localhost:4000/api/createRole", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nitrogen,
-          phosphorus,
-          potassium,
-        }),
-      });
+ // Handle Submit สำหรับฟอร์ม Factor
+ const handleSubmitFactor = async (e) => {
 
-      if (res.ok) {
-        toast.success("เพิ่มข้อมูล Factor เรียบร้อยแล้ว");
-        setRefresh(true);
-        onOpenChange(false);
-      } else {
-        toast.warn("เพิ่มข้อมูล Factor ล้มเหลว");
-      }
-    } catch (err) {
-      console.log("Error:", err);
+  console.log({
+    ph,
+    temperature,
+    humidity,
+    lightIntensity,
+    salinity,
+    plantId,  // ตรวจสอบว่า plantId มีค่าหรือไม่
+  });
+
+  e.preventDefault();
+
+  if (!ph || !temperature || !humidity || !lightIntensity || !salinity || !plantId) {
+    toast.error("กรุณากรอกข้อมูลให้ครบทุกช่อง!");
+    return;
+  }
+
+  try {
+    const res = await fetch("http://localhost:4000/api/createFactor", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ph,
+        temperature,
+        humidity,
+        lightIntensity,
+        salinity,
+        plantId,  // ส่ง id ของพืชไปแทนชื่อพืช
+        
+      }),
+    });
+
+    if (res.ok) {
+      toast.success("เพิ่มข้อมูล Factor เรียบร้อยแล้ว");
+      setRefresh(true);
+      onOpenChange(false);
+    } else {
+      toast.warn("เพิ่มข้อมูล Factor ล้มเหลว");
     }
-  };
+  } catch (err) {
+    console.log("Error:", err);
+  }
+};
 
-  // Handle Submit สำหรับ Form 2
-  const handleSubmitNutrient = async (e) => {
-    e.preventDefault();
+ // Handle Submit สำหรับฟอร์ม Nutrient
+ const handleSubmitNutrient = async (e) => {
+  e.preventDefault();
 
-    if (!ph || !temperature || !humidity || !lightIntensity || !salinity) {
-      toast.error("กรุณากรอกข้อมูลให้ครบทุกช่อง!");
-      return;
+  if (!nitrogen || !phosphorus || !potassium || !plantId) {
+    toast.error("กรุณากรอกข้อมูลให้ครบทุกช่อง!");
+    return;
+  }
+
+  try {
+    const res = await fetch("http://localhost:4000/api/createNutrient", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nitrogen,
+        phosphorus,
+        potassium,
+        plantId,  // ส่ง id ของพืชไปแทนชื่อพืช
+      }),
+    });
+
+    if (res.ok) {
+      toast.success("เพิ่มข้อมูล Nutrient เรียบร้อยแล้ว");
+      setRefresh(true);
+      onOpenChange(false);
+    } else {
+      toast.warn("เพิ่มข้อมูล Nutrient ล้มเหลว");
     }
+  } catch (err) {
+    console.log("Error:", err);
+  }
+};
 
-    try {
-      const res = await fetch("http://localhost:4000/api/createRole", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ph,
-          temperature,
-          humidity,
-          lightIntensity,
-          salinity,
-        }),
-      });
+return (
+  <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+    <ModalContent>
+      <ModalHeader className="flex flex-col gap-1">เพิ่มข้อมูล</ModalHeader>
+      <ModalBody>
+        <Tabs aria-label="Options">
+          <Tab key="Factor" title="Factor">
+            <form onSubmit={handleSubmitFactor}>
+              <div className="flex mb-4 gap-4">
+                <Input
+                  onChange={(e) => setPh(e.target.value)}
+                  type="text"
+                  label="pH"
+                  isRequired
+                />
+                <Input
+                  onChange={(e) => setTemperature(e.target.value)}
+                  type="text"
+                  label="Temperature"
+                  isRequired
+                />
+              </div>
+              <div className="flex my-4 gap-4">
+                <Input
+                  onChange={(e) => setHumidity(e.target.value)}
+                  type="text"
+                  label="Humidity"
+                  isRequired
+                />
+                  <Input
+                  onChange={(e) =>  setLightIntensity(e.target.value)}
+                  type="text"
+                  label="LightIntensity"
+                  isRequired
+                />
+              </div>
+              <div className="flex my-4 gap-4">
+                <Input
+                  onChange={(e) =>  setSalinity(e.target.value)}
+                  type="text"
+                  label="Salinity"
+                  isRequired
+                />
+              </div>
+              <div className="flex my-4 gap-4">
+                <Input
+                    label="พืช"
+                    variant="bordered"
+                    value={plantName}
+                    onChange={(e) => setPlantName(e.target.value)}
+                    isReadOnly 
+                />
+              </div>
+              <ModalFooter>
+                <Button variant="flat" onPress={onOpenChange}>ยกเลิก</Button>
+                <Button type="submit" color="primary">เพิ่ม</Button>
+              </ModalFooter>
+            </form>
+          </Tab>
 
-      if (res.ok) {
-        toast.success("เพิ่มข้อมูล Nutrient เรียบร้อยแล้ว");
-        setRefresh(true);
-        onOpenChange(false);
-      } else {
-        toast.warn("เพิ่มข้อมูล Nutrient ล้มเหลว");
-      }
-    } catch (err) {
-      console.log("Error:", err);
-    }
-  };
-
-  return (
-    
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">เพิ่มข้อมูล</ModalHeader>
-            <ModalBody>
-              <Tabs aria-label="Options">
-                <Tab key="Factor" title="Factor">
-                  {/* Form 1: Factor */}
-                  <form onSubmit={handleSubmitFactor}>
-                    <div className="flex mb-4 gap-4">
-                      <Input
-                        onChange={(e) => setNitrogen(e.target.value)}
-                        type="text"
-                        label="Nitrogen"
-                        isRequired
-                      />
-                      <Input
-                        onChange={(e) => setPhosphorus(e.target.value)}
-                        type="text"
-                        label="Phosphorus"
-                        isRequired
-                      />
-                    </div>
-                    <div className="flex my-4 gap-4">
-                      <Input
-                        onChange={(e) => setPotassium(e.target.value)}
-                        type="text"
-                        label="Potassium"
-                        isRequired
-                      />
-                    </div>
-                    <ModalFooter>
-                      <Button variant="flat" onPress={onClose}>
-                        ยกเลิก
-                      </Button>
-                      <Button type="submit" color="primary">
-                        เพิ่ม
-                      </Button>
-                    </ModalFooter>
-                  </form>
-                </Tab>
-
-                <Tab key="Nutrient" title="Nutrient">
-                  {/* Form 2: Nutrient */}
-                  <form onSubmit={handleSubmitNutrient}>
-                    <div className="flex mb-4 gap-4">
-                      <Input
-                        onChange={(e) => setPh(e.target.value)}
-                        type="text"
-                        label="pH"
-                        isRequired
-                      />
-                      <Input
-                        onChange={(e) => setTemperature(e.target.value)}
-                        type="text"
-                        label="Temperature"
-                        isRequired
-                      />
-                    </div>
-                    <div className="flex my-4 gap-4">
-                      <Input
-                        onChange={(e) => setHumidity(e.target.value)}
-                        type="text"
-                        label="Humidity"
-                        isRequired
-                      />
-                      <Input
-                        onChange={(e) => setLightIntensity(e.target.value)}
-                        type="text"
-                        label="Light Intensity"
-                        isRequired
-                      />
-                    </div>
-                    <div className="flex my-4 gap-4">
-                      <Input
-                        onChange={(e) => setSalinity(e.target.value)}
-                        type="text"
-                        label="Salinity"
-                        isRequired
-                      />
-                    </div>
-                    <ModalFooter>
-                      <Button variant="flat" onPress={onClose}>
-                        ยกเลิก
-                      </Button>
-                      <Button type="submit" color="primary">
-                        เพิ่ม
-                      </Button>
-                    </ModalFooter>
-                  </form>
-                </Tab>
-              </Tabs>
-            </ModalBody>
-          </>
-        )}
+          <Tab key="Nutrient" title="Nutrient">
+              {/* ฟอร์มสำหรับ Nutrient */}
+              <form onSubmit={handleSubmitNutrient}>
+                <div className="flex mb-4 gap-4">
+                  <Input
+                    onChange={(e) => setNitrogen(e.target.value)}
+                    type="text"
+                    label="Nitrogen"
+                    isRequired
+                  />
+                  <Input
+                    onChange={(e) => setPhosphorus(e.target.value)}
+                    type="text"
+                    label="Phosphorus"
+                    isRequired
+                  />
+                </div>
+                <div className="flex my-4 gap-4">
+                  <Input
+                    onChange={(e) => setPotassium(e.target.value)}
+                    type="text"
+                    label="Potassium"
+                    isRequired
+                  />
+                </div>
+                <div className="flex my-4 gap-4">
+                  <Input
+                      label="พืช"
+                      variant="bordered"
+                      value={plantName}
+                      onChange={(e) => setPlantName(e.target.value)}
+                      isReadOnly 
+                  />
+                </div>
+                <ModalFooter>
+                  <Button variant="flat" onPress={onOpenChange}>ยกเลิก</Button>
+                  <Button type="submit" color="primary">เพิ่ม</Button>
+                </ModalFooter>
+              </form>
+            </Tab>
+          </Tabs>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
