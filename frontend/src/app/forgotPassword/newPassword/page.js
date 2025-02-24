bf282c96d303df1c31cc6ab1700bb67b79bff5e0
Chayathon <gusgus157903@gmail.com>
2025-02-25 02:21:18 +0700

    Update forgotPassword & Add newPassword
    check email
    gen otp
    check otp
    create new password

---
+++ frontend/src/app/forgotPassword/newPassword/page.js
@@ -0,0 +1,108 @@
+"use client";
+
+import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+
+function NewPassword() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const email = searchParams.get("email");
+
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!password || !confirmPassword) {
+            toast.error("กรุณากรอกข้อมูลให้ครบทุกช่อง!");
+            return;
+        }
+
+        if(password !== confirmPassword) {
+            toast.error("รหัสผ่านไม่ตรงกัน!");
+            return;
+        }
+
+        try {
+            const res = await fetch(`http://localhost:4000/api/updatePassword`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email, password
+                })
+            });
+
+            if(res.status === 200) {
+                toast.success("เปลี่ยนรหัสผ่านสำเร็จแล้ว");
+
+                setTimeout(() => {
+                    router.push('/');
+                }, 1000)
+            } else {
+                toast.error("เปลี่ยนรหัสผ่านไม่สำเร็จ!");
+            }
+        } catch(error) {
+            console.error("Error: ", error);
+        }
+    }
+
+    return (
+        <div className="grid w-[100vw] h-[100vh]">
+            <Card className="m-auto w-1/3 drop-shadow-2xl bg-blend-darken">
+                <CardHeader className='text-2xl font-bold justify-center'>
+                    สร้างรหัสผ่านใหม่
+                </CardHeader>
+                <CardBody>
+                    <form onSubmit={handleSubmit}>
+                        <div className='my-4'>
+                            <Input
+                                onChange={(e) => setPassword(e.target.value)}
+                                label="รหัสผ่าน"
+                                endContent={
+                                    <Button type="button" size="sm" className='bg-gray-300' onPress={toggleVisibility} aria-label="toggle password visibility">
+                                    {isVisible ? (
+                                        'ซ่อน'
+                                    ) : (
+                                        'แสดง'
+                                    )}
+                                    </Button>
+                                }
+                                type={isVisible ? "text" : "password"}
+                                autoFocus
+                                isRequired
+                            />
+                        </div>
+                        <div className='my-4'>
+                            <Input
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                label="ยืนยันรหัสผ่าน"
+                                endContent={
+                                    <Button type="button" size="sm" className='bg-gray-300' onPress={toggleVisibility} aria-label="toggle password visibility">
+                                    {isVisible ? (
+                                        'ซ่อน'
+                                    ) : (
+                                        'แสดง'
+                                    )}
+                                    </Button>
+                                }
+                                type={isVisible ? "text" : "password"}
+                                isRequired
+                            />
+                        </div>
+                        <Button type='submit' color='primary' className='w-full'>ยืนยัน</Button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
+export default NewPassword
--- frontend/src/app/forgotPassword/page.js
+++ frontend/src/app/forgotPassword/page.js
@@ -1,30 +1,90 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
-import {Card, CardHeader, CardBody, CardFooter, Input, Button} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Input, Button, InputOtp } from "@nextui-org/react";
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
-function Page() {
-  return(
-    <div className="grid w-[100vw] h-[100vh]">
-        <Card className="m-auto w-1/3 drop-shadow-2xl bg-blend-darken">
-            <CardHeader className='text-2xl font-bold justify-center'>
-                ลืมรหัสผ่าน
-            </CardHeader>
-            <CardBody>
-                <form>
-                    <div className='mb-4'>
-                        <Input type='email' label='Email' variant='faded' />
-                    </div>
-                    <Button color='primary' className='w-full'>ยืนยัน</Button>
-                </form>
-            </CardBody>
-            <CardFooter>
-                กลับไปหน้า&nbsp;<Link href='/' className='text-blue-500 hover:underline'>เข้าสู่ระบบ</Link>
-            </CardFooter>
-        </Card>
-    </div>
-  )
+function ForgotPassword() {
+    const router = useRouter();
+
+    const [email, setEmail] = useState('');
+    const [otp, setOTP] = useState();
+    const [inputOTP, setInputOTP] = useState();
+    const [isCheck, setIsCheck] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            if (!isCheck) {
+                const res = await fetch(`http://localhost:4000/api/getUserbyEmail/${email}`);
+
+                if(res.status === 200) {
+                    const data = await res.json();
+                    setOTP(data.resultData.otp);
+                    setIsCheck(true);
+                    
+                    toast.success("ส่ง OTP ไปที่อีเมลของคุณแล้ว")
+                } else if(res.status === 404) {
+                    toast.error("ไม่พบที่อยู่อีเมลนี้!")
+                }
+            } else {
+                if(inputOTP == otp) {
+                    router.push(`/forgotPassword/newPassword?email=${encodeURIComponent(email)}`);
+                } else {
+                    toast.warn("OTP ไม่ถูกต้อง!");
+                }
+            }
+        } catch (error) {
+            console.error("Failed to fetch: ", error);
+        }
+    }
+
+    return (
+        <div className="grid w-[100vw] h-[100vh]">
+            <Card className="m-auto w-1/3 drop-shadow-2xl bg-blend-darken">
+                <CardHeader className='text-2xl font-bold justify-center'>
+                    ลืมรหัสผ่าน
+                </CardHeader>
+                <CardBody>
+                    <form onSubmit={handleSubmit}>
+                        <div className='mb-2'>
+                            <Input
+                                onChange={(e) => setEmail(e.target.value)}
+                                type='email'
+                                label='อีเมล'
+                                variant='faded'
+                                autoFocus
+                                isRequired
+                            />
+                        </div>
+                        <div className='m-2'>
+                            {isCheck && (
+                                <>
+                                    <p>กรุณากรอก OTP</p>
+                                    <InputOtp
+                                        isRequired
+                                        autoFocus
+                                        onChange={(e) => setInputOTP(e.target.value)}
+                                        aria-label="OTP input field"
+                                        length={6}
+                                        name="otp"
+                                        placeholder="Enter code"
+                                    />
+                                </>
+                            )}
+                        </div>
+                        <Button type='submit' color='primary' className='w-full'>ยืนยัน</Button>
+                    </form>
+                </CardBody>
+                <CardFooter>
+                    กลับไปหน้า&nbsp;<Link href='/' className='text-blue-500 hover:underline'>เข้าสู่ระบบ</Link>
+                </CardFooter>
+            </Card>
+        </div>
+    )
 }
 
-export default Page
+export default ForgotPassword
