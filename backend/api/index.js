a4c45c4f4ee389f36a5c6aadb142632dca29bf01
Chayathon <gusgus157903@gmail.com>
2025-05-14 00:47:21 +0700

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const serverless = require('serverless-http');
-
 const app = express();
+const serverless = require('serverless-http');
 
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
+
+app.use('/api', require('./router/auth'));
+app.use('/api', require('./router/address'));
+app.use('/api', require('./router/user'));
+app.use('/api', require('./router/admin'));
+app.use('/api', require('./router/role'));
+app.use('/api', require('./router/plant'));
+app.use('/api', require('./router/plant_variable'));
+app.use('/api', require('./router/factor_nutrient'));
 
 module.exports.handler = serverless(app);
