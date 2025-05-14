3cdc6659fdd791ebe8bc388b1729c13566afa4a9
Chayathon <gusgus157903@gmail.com>
2025-05-14 15:03:59 +0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
 
 app.use('/api', require('./router/auth'));
