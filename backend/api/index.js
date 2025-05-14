03723098d8718b7d44f1f5bed12e59e7934082be
Chayathon <gusgus157903@gmail.com>
2025-05-14 14:12:24 +0700

    update

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,13 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
-app.use('/api', require('./router/auth'));
-app.use('/api', require('./router/address'));
-app.use('/api', require('./router/user'));
-app.use('/api', require('./router/admin'));
-app.use('/api', require('./router/role'));
-app.use('/api', require('./router/plant'));
-app.use('/api', require('./router/plant_variable'));
-app.use('/api', require('./router/factor_nutrient'));
-
 module.exports.handler = serverless(app);
