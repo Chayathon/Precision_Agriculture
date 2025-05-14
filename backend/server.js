e987a3a6327579d66beab83ea6f3413627e5cc56
Chayathon <gusgus157903@gmail.com>
2025-05-14 14:50:57 +0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(cors());
-const serverless = require('serverless-http');
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
+const PORT = process.env.PORT || 4000;
 
 app.use('/api', require('./router/auth'));
 app.use('/api', require('./router/address'));
@@ -22,7 +14,7 @@ app.use('/api', require('./router/plant_variable'));
 app.use('/api', require('./router/factor_nutrient'));
 
 app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
