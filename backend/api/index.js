8d40d53d31eebf99b7d3ebc2a56971f3157fab41
Chayathon <gusgus157903@gmail.com>
2025-05-14 14:26:23 +0700

    Update

--- backend/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const serverless = require('serverless-http');
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
-
-module.exports.handler = serverless(app);
